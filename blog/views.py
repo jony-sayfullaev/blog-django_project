00dd1a54fd32eb16b353ea1780300a2608841ae9
jony-sayfullaev <jonicks2000@gmail.com>
2022-12-11 07:19:50 -0500

    adding URLs and Views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def starting_page(request):
+    # return render(request,"")
+    pass
+
+
+def posts(request):
+    # return render(request,"")
+    pass
+
+
+def post_details(request):
+    # return render(request,"")
+    pass
