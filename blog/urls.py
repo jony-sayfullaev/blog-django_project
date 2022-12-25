d37a63ab0ef8b37fa82583901251ccd79515db27
jony-sayfullaev <jonicks2000@gmail.com>
2022-12-25 08:26:41 -0500

    adding read-later page & managing with via sessions

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.StartingPageView.as_view(), name='starting_page'),
     path('posts', views.PostsView.as_view(), name='posts_page'),
-    path("posts/<slug:slug>", views.PostDetailView.as_view(), name='post_detail_page')
+    path("posts/<slug:slug>", views.PostDetailView.as_view(),
+         name='post_detail_page'),
+    path("read-later", views.ReadLaterView.as_view(), name='read-later')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.urls import reverse
 from .forms import CommentForm
-from django.views.generic.base import TemplateView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -50,12 +52,60 @@ class PostsView(ListView):
 #     })
 
 
-class PostDetailView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class PostDetailView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by('-id')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+        }
+        return render(request, 'blog/post-detail.html', context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse('post_detail_page', args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by('-id')
+        }
+        return render(request, 'blog/post-detail.html', context)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        return render(request, "blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+        return HttpResponseRedirect('/')
